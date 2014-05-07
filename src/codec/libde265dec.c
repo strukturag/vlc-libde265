fae2d068f1c9523cbad0ce8fa1a6f94174a9b9c1
Joachim Bauch <bauch@struktur.de>
2014-05-07 17:44:06 +0200

    Internationalize shortname.

--- src/codec/libde265dec.c
+++ src/codec/libde265dec.c
@@ -62,7 +62,7 @@ static void Close(vlc_object_t *);
  *****************************************************************************/
 
 vlc_module_begin ()
-    set_shortname("libde265dec")
+    set_shortname(N_("libde265dec"))
     set_description(N_("HEVC/H.265 video decoder using libde265"))
     set_capability("decoder", 200)
     set_callbacks(Open, Close)
